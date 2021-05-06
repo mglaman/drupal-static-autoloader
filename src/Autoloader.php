c5e3657de02b22ff6c4b4fff6c8220431cbc6b64
Matt Glaman <nmd.matt@gmail.com>
2021-05-06 00:55:08 -0500

    Autoloading of test namespaces

--- src/Autoloader.php
+++ src/Autoloader.php
@@ -77,10 +77,27 @@ final class Autoloader
         if ($type !== 'module' && $type !== 'theme' && $type !== 'profile' && $type !== 'theme_engine') {
             throw new \InvalidArgumentException("Must be 'module' or 'theme' but got $type");
         }
+        // Taken from drupal_phpunit_get_extension_namespaces.
+        $suite_names = ['Unit', 'Kernel', 'Functional', 'Build', 'FunctionalJavascript'];
         $extensions = $this->extensionDiscovery->scan($type);
         foreach ($extensions as $extension_name => $extension) {
             $path = $this->drupalRoot . '/' . $extension->getPath() . '/src';
             $this->autoloader->addPsr4("Drupal\\{$extension_name}\\", $path);
+
+            // @see drupal_phpunit_get_extension_namespaces().
+            $test_dir = $this->drupalRoot . '/' . $extension->getPath() . '/tests/src';
+            foreach ($suite_names as $suite_name) {
+                $suite_dir = $test_dir . '/' . $suite_name;
+                if (is_dir($suite_dir)) {
+                    $this->autoloader->addPsr4("Drupal\\Tests\{$extension_name}\\$suite_name\\", $path);
+                }
+            }
+            // Extensions can have a \Drupal\extension\Traits namespace for
+            // cross-suite trait code.
+            $trait_dir = $test_dir . '/Traits';
+            if (is_dir($trait_dir)) {
+                $this->autoloader->addPsr4('Drupal\\Tests\\' . $extension_name . '\\Traits\\', $trait_dir);
+            }
 
             $this->loadExtension($extension);
         }
